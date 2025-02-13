3d28e2f679296a44b60e95ac5e7ccdcc0106f2e1
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-02-13 18:31:46 -0500

    Added stubs for Course.java.  Currently does not compile due to missing Professor class

---
+++ src/main/java/edu/gcc/comp350/Course.java
@@ -0,0 +1,70 @@
+package edu.gcc.comp350;
+
+import java.sql.Time;
+
+public class Course {
+
+    private int id;
+    private Professor professor;
+    private Main.Session session;
+    private Time startTime;
+    private Time endTime;
+    private Main.Days courseDays;
+    private String courseDept;
+    private String courseCode;
+    private int referenceCode;
+    private String description;
+
+    protected Course(Professor professor,
+                     Main.Session session,
+                     Time startTime,
+                     Time endTime,
+                     Main.Days courseDays,
+                     String courseDept,
+                     String courseCode,
+                     int referenceCode,
+                     String description) {
+
+    }
+
+    protected Professor getProfessor() {
+        return null;
+    }
+
+    protected Main.Session getSession() {
+        return null;
+    }
+
+    protected Time getStartTime() {
+        return null;
+    }
+
+    protected Time getEndTime() {
+        return null;
+    }
+
+    protected Main.Days getCourseDays() {
+        return null;
+    }
+
+    protected String getCourseDept() {
+        return null;
+    }
+
+    protected String getCourseCode() {
+        return null;
+    }
+
+    protected int getReferenceCode() {
+        return -1;
+    }
+
+    protected String getDescription() {
+        return null;
+    }
+
+    protected int getId() {
+        return -1;
+    }
+
+}
