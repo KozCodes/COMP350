0310d0168d21ce3478447d44b349068ccc00a67a
KozCodes <kozorajc23@gcc.edu.com>
2025-02-13 16:24:49 -0500

    First

---
+++ src/main/java/edu/gcc/comp350/Launcher.java
@@ -0,0 +1,13 @@
+package edu.gcc.comp350;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println(myFavoriteNumber());
+    }
+
+    protected static int myFavoriteNumber() {
+        return 42;
+    }
+
+
+}
---
+++ src/test/java/edu/gcc/comp350/LauncherTest.java
@@ -0,0 +1,9 @@
+package edu.gcc.comp350;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LauncherTest {
+    void myFavoriteNumber() {
+        assertEquals(42, Launcher.myFavoriteNumber());
+    }
+}
