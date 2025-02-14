ce1f22bbf6373181951c3cf8a4cc19059e483fc7
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-02-13 19:23:39 -0500

    Added stubs for Signup.java

---
+++ src/main/java/edu/gcc/comp350/Signup.java
@@ -0,0 +1,18 @@
+package edu.gcc.comp350;
+
+import java.util.List;
+
+public class Signup {
+    private int id;
+    private String name;
+    private String major;
+    private List<String> minors;
+
+    protected Signup(String name, String major, List<String> minors) {
+
+    }
+
+    protected void signUp(Student student) {
+
+    }
+}
