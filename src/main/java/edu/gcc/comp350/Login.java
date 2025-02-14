0ba38f095f0e43cf28d9972c5301fc3fa31c0d67
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-02-13 19:14:56 -0500

    Added stubs for Login.java

---
+++ src/main/java/edu/gcc/comp350/Login.java
@@ -0,0 +1,15 @@
+package edu.gcc.comp350;
+
+public class Login {
+
+    private int id;
+    private String name;
+
+    protected Student getStudent(int id, String name) {
+        return null;
+    }
+
+    protected void login(Student student) {
+
+    }
+}
