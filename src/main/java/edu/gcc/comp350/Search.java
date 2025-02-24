2b7174970aa26bb4bc766755d7ebc056f2e11851
ERROREVEN <126495284+ERROREVEN@users.noreply.github.com>
2025-02-24 15:49:31 -0500

    added things to search

--- src/main/java/edu/gcc/comp350/Search.java
+++ src/main/java/edu/gcc/comp350/Search.java
@@ -1,5 +1,6 @@
 package edu.gcc.comp350;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Search {
@@ -10,7 +11,8 @@ public class Search {
     private List<Course> filteredResults;
 
     protected Search(String query, Filter filter) {
-
+        this.query = query;
+        this.filter = filter;
     }
 
     protected void search(String query) {
@@ -18,7 +20,12 @@ public class Search {
     }
 
     protected void applyFilter(Filter filter) {
+       List<Course> temp = new ArrayList<>();
 
+
+        for (Course course : temp) {
+
+        }
     }
 
     protected void modifyFilter(Filter filter) {
@@ -26,19 +33,20 @@ public class Search {
     }
 
     protected void queryChange(String query) {
-
+        this.query = query;
+        search(query);
     }
 
     protected List<Course> getSearchResults() {
-        return null;
+        return searchResults;
     }
 
     protected List<Course> getFilteredResults() {
-        return null;
+        return filteredResults;
     }
 
     protected Filter getFilter() {
-        return null;
+        return filter;
     }
 
 
--- src/test/java/edu/gcc/comp350/LauncherTest.java
+++ src/test/java/edu/gcc/comp350/LauncherTest.java
@@ -3,7 +3,5 @@ package edu.gcc.comp350;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LauncherTest {
-    void myFavoriteNumber() {
-        assertEquals(42, Launcher.myFavoriteNumber());
-    }
+    
 }
