454927089e6179551514be1187202dd2831c1a81
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-02-24 21:55:03 -0500

    Revert "Added random ids, Javadoc for constructors"
    This reverts commit 362fe566ae31d37ba4687f41364a95f12594b69d.

--- src/main/java/edu/gcc/comp350/Schedule.java
+++ src/main/java/edu/gcc/comp350/Schedule.java
@@ -2,7 +2,6 @@ package edu.gcc.comp350;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Stack;
 
 public class Schedule {
@@ -12,31 +11,17 @@ public class Schedule {
     private String name;
     private Stack<Course> lastChangedCourses;
 
-    /**
-     * Schedule Constructor
-     * Class variable id is initialized to a random number [1, 6000)
-     * Class variable classes is initialized to an empty ArrayList
-     * Class variable lastChangedCourses is initialized to a new Stack
-     * Class variable name is initialized to "Schedule " + 1 + the number of schedules the current student currently has
-     */
+    // constructor
     protected Schedule() {
-        this.id = new Random().nextInt(6000);
+        this.id = Main.currentStudent.getSchedules().size();
         this.classes = new ArrayList<>();
         this.lastChangedCourses = new Stack<>();
-        this.name = "Schedule " + (Main.currentStudent.getSchedules().isEmpty() ? 1 : Main.currentStudent.getSchedules().size() + 1);
+        this.name = "Schedule " + this.id;
     }
 
-    /**
-     * Schedule Constructor
-     * Class variable id is initialized to a random number [1, 6000)
-     * Class variable classes is initialized to an empty ArrayList
-     * Class variable lastChangedCourses is initialized to a new Stack
-     * Class variable name is initialized to param name
-     *
-     * @param name String specified name for this Schedule
-     */
+    // overloaded constructor
     protected Schedule(String name) {
-        this.id = new Random().nextInt(6000);
+        this.id = Main.currentStudent.getSchedules().size();
         this.classes = new ArrayList<>();
         this.lastChangedCourses = new Stack<>();
         this.name = name;
