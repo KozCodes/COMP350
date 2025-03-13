c67dc8aed9cfbdf5095e0cda4de8fec4a30f1636
Rvz4 <soderlundjs22@gcc.edu>
2025-03-13 14:56:40 -0400

    Added database driver dependency and method for connection to the database

--- src/main/java/edu/gcc/comp350/Main.java
+++ src/main/java/edu/gcc/comp350/Main.java
@@ -1,8 +1,12 @@
 package edu.gcc.comp350;
 
 import java.util.List;
+import  java.sql.*;
 
 public class Main {
+    public Main() {
+    }
+
     protected static enum Days {
         MWF,
         TR
@@ -22,11 +26,41 @@ public class Main {
     protected Search search;
     protected Schedule currentSchedule;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        onLoad();
     }
 
-    protected static void onLoad() {
+    protected static void onLoad() throws SQLException, ClassNotFoundException {
+        connect();
+    }
 
+    public static void connect() {
+        // connection string
+        var url = "jdbc:sqlite:C:/Users/SODERLUNDJS22/comp350/Scrumbucket/Database/scrumbucketData.db";
+
+        var sql = "SELECT id, professor, session, startTime, endTime, courseDays, courseDept, courseCode, referenceCode, description FROM courses";
+
+        try (var conn = DriverManager.getConnection(url);
+             var stmt = conn.createStatement();
+             var rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("id") +
+                        " | " + rs.getString("professor") +
+                        " | " + rs.getInt("session") +
+                        " | " + rs.getString("startTime") +
+                        " | " + rs.getString("endTime") +
+                        " | " + rs.getString("courseDays") +
+                        " | " + rs.getString("courseDept") +
+                        " | " + rs.getString("courseCode") +
+                        " | " + rs.getInt("referenceCode") +
+                        " | " + rs.getString("description")
+                );
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     protected static void displaySchedule(Schedule schedule) {
