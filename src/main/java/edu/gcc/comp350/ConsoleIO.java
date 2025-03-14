1c3560b8b46317698900eddf2074e73128ba26c1
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-03-13 20:05:47 -0400

    Stub methods for commands

--- src/main/java/edu/gcc/comp350/ConsoleIO.java
+++ src/main/java/edu/gcc/comp350/ConsoleIO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleIO {
-    List<String> commands = Arrays.asList("add", "remove", "save", "load", "delete", "new", "filter", "search");
+    List<String> commands = Arrays.asList("add", "remove", "save", "load", "delete", "new", "filter", "search", "printClass", "schedule");
 
     /**
      * runs testing cycle for console input
@@ -31,6 +31,85 @@ public class ConsoleIO {
     }
 
     /**
+     * add course instance to current schedule
+     * @param referenceNumber int reference number of course instance
+     */
+    private void add(int referenceNumber) {
+
+    }
+
+    /**
+     * removes course instance from current schedule
+     * @param referenceNumber int reference number of course instance
+     */
+    private void remove(int referenceNumber) {
+
+    }
+
+    /**
+     * saves current schedule to database
+     */
+    private void save() {
+
+    }
+
+    /**
+     * loads schedule
+     * @param schedule int id of desired schedule
+     */
+    private void load(int schedule) {
+
+    }
+
+    /**
+     * deletes schedule from database
+     * @param schedule int id of desired schedule
+     */
+    private void delete(int schedule) {
+
+    }
+
+    /**
+     * makes new empty schedule for student
+     * this schedule becomes current schedule
+     */
+    private void newSchedule() {
+
+    }
+
+    /**
+     * edit search filter option to given value
+     * @param type String option to be edited
+     * @param val String new value to set option to
+     */
+    private void filter(String type, String val) {
+
+    }
+
+    /**
+     * searches and prints results based on keywords
+     * @param keywords String to search by
+     */
+    private void search(String keywords) {
+
+    }
+
+    /**
+     * prints information of desired course
+     * @param referenceNumber int id of desired schedule
+     */
+    private void printCourse(int referenceNumber) {
+
+    }
+
+    /**
+     * print contents of current schedule
+     */
+    private void schedule() {
+
+    }
+
+    /**
      * print all commands and syntax to console
      */
     private void help() {
@@ -43,5 +122,7 @@ public class ConsoleIO {
         System.out.println("new");
         System.out.println("filter <filter type> <filter value>");
         System.out.println("search <keyword string>");
+        System.out.println("printCourse <referenceNumber>");
+        System.out.println("schedule");
     }
 }
