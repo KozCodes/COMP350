296fb456977005475f5162136a6b1d57dc32c5cf
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2025-03-17 15:19:31 -0400

    Filled in logic for add and remove courses CJJS-1 and CJJS-5

--- src/main/java/edu/gcc/comp350/Schedule.java
+++ src/main/java/edu/gcc/comp350/Schedule.java
@@ -42,23 +42,75 @@ public class Schedule {
         this.name = name;
     }
 
+    /**
+     * Get the database ID of the Schedule object
+     * @return
+     */
     protected int getId() {
-        return -1;
+        return id;
     }
 
+    /**
+     * Get the current name of the Schedule
+     * @return String name of the Schedule
+     */
     protected String getName() {
-        return null;
+        return name;
     }
 
-    protected void setName() {
-
+    /**
+     * Set the name of the Schedule
+     * @param name String new name for the Schedule
+     */
+    protected void setName(String name) {
+        this.name = name;
     }
 
+    /**
+     * Add a course to the Schedule
+     * @param courseID int ID of the course to add
+     */
     protected void addCourse(int courseID) {
-
+        // find course in db
+        Course course = null;
+        // add course to classes
+        classes.add(course);
+        // add course to lastChangedCourses
+        lastChangedCourses.push(course);
     }
 
-    protected void undo() {
+    /**
+     * Remove a course from the Schedule
+     * @param courseID int ID of the course to remove
+     */
+    protected void removeCourse(int courseID) {
+        // find course in db
+        Course course = null;
+        // remove course from classes
+        classes.remove(course);
+        // add course to lastChangedCourses
+        lastChangedCourses.push(course);
+    }
 
+    /**
+     * Undo the last change made to the Schedule
+     * If the top of the lastChangedCourses stack is in the classes list, remove it
+     * Else, add it to the classes list
+     */
+    protected void undo() {
+        Course course = lastChangedCourses.pop();
+        if (classes.contains(course)) {
+            classes.remove(course);
+        } else {
+            classes.add(course);
+        }
+    }
+
+    /**
+     * Get the list of courses in the Schedule
+     * @return List<Course> list of courses in the Schedule
+     */
+    protected List<Course> getCourses() {
+        return classes;
     }
 }
