08834ebcaa9a89d825fa24ee06b5f8c3ab5273bd
Rvz4 <soderlundjs22@gcc.edu>
2025-04-14 12:25:54 -0400

    Init autoScheduler

---
+++ src/main/java/edu/gcc/comp350/AutoScheduler.java
@@ -0,0 +1,23 @@
+package edu.gcc.comp350;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AutoScheduler {
+    protected ArrayList<Schedule> generateSections(ArrayList<String> courses, int numSchedueles) {
+        System.out.println("Enter courses");
+        for(String course : courses) {
+            // Get courses from object in main and check if entered course is in that list
+        }
+
+        return null;
+    }
+
+    protected ArrayList<Schedule> generateCourses(ArrayList<String> courses, int numSchedueles) {
+        return null;
+    }
+
+    protected ArrayList<Schedule> generateCourses(ArrayList<String> courses, int numSchedueles, HashMap<String, ArrayList<Course>> coursePrerequisites, HashMap<String, Boolean> courseIsRequired, HashMap<String, Integer> courseYearsOffered) {
+        return null;
+    }
+}
