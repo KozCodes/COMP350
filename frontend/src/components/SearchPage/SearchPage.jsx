3ae0c877ac66965a24126cbee340c0e054b94d31
ERROREVEN <126495284+ERROREVEN@users.noreply.github.com>
2025-04-24 16:16:28 -0400

    more search stuff

--- frontend/src/components/SearchPage/SearchPage.jsx
+++ frontend/src/components/SearchPage/SearchPage.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-
 const CourseSearch = () => {
 
     const [query, setQuery] = useState('');
@@ -22,9 +21,16 @@ const CourseSearch = () => {
            }
   }
 
+
   return (
-   <div style={{textAlign: 'center', padding: '2rem', minHeight: '100vh', color: 'black' }}>
+      <div style={{textAlign: 'left', padding: '2rem'}}>
+   <button style = {{fontWeight: 'bold', backgroundColor: '#990000', color: 'white'}} onClick={() => navigate('/Home')}> Home
+   </button>
+   <button style = {{fontWeight: 'bold', backgroundColor: '#990000', color: 'white'}} onClick={() => navigate('/Home')}> See Schedule
+      </button>
+   <div style={{textAlign: 'center', padding: '4rem', minHeight: '100vh', color: 'black' }}>
    <h2 style = {{ fontWeight: 'bold', color: '#990000'}}> Course Search </h2>
+   <div style= {{textAlign: 'center', padding: '2rem'}}>
    <input style = {{}}
    name = "Search..."
    value = {query}
@@ -33,9 +39,24 @@ const CourseSearch = () => {
    />
    <button type= "submit"
         onClick={() => handleSearch()}> Search
-    </button>
+   </button>
    <h1> {message}</h1>
-  </div>
+   <div style={{textAlign: 'center', padding: '2rem', minHeight: '100vh', color: 'black' }}>
+   <h1> Filter Your Search </h1>
+   <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+    <label> Filter by Days </label>
+    
+   <label> Filter by Times </label>
+    <label> Filter by Session </label>
+    <label> Filter by Year </label>
+    <label> Filter by Course Code </label>
+    <label> Filter by Course Department </label>
+    <label> Filter by Course Year </label>
+    </div>
+   </div>
+   </div>
+   </div>
+   </div>
   );
 };
 
