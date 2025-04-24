508bba6e2c62065896788a920dc9ea9ef69cf431
Rvz4 <soderlundjs22@gcc.edu>
2025-04-23 20:25:31 -0400

    Merged with Master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import Schedule from "./components/Schedule/schedule";
 import ProfessorRatingPage from "./components/ProfessorRater/ProfessorRatingPage";
 import HomePage from "./components/HomePage/HomePage";
 import SearchPage from './components/SearchPage/SearchPage';
-
 import SchedulerPage from "./components/SchedulerPage/SchedulerPage"; // npm install @dnd-kit/core @dnd-kit/sortable @dnd-kit/utilities
 import 'bootstrap/dist/css/bootstrap.min.css'; // npm install bootstrap
 
--- src/main/java/edu/gcc/comp350/RESTController.java
+++ src/main/java/edu/gcc/comp350/RESTController.java
@@ -13,6 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
